41e8d1dff8f120ac857e249f06009699bbb56aa2
chansung <pork1375@gmail.com>
2023-11-19 12:50:46 +0900

    섹션3-2 프론트 컨트롤러 도입

--- src/test/java/com/example/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/com/example/servlet/domain/member/MemberRepositoryTest.java
@@ -41,7 +41,7 @@ class MemberRepositoryTest {
         memberRepository.save(member2);
 
         // when
-        List<Member> result = memberRepository.findALl();
+        List<Member> result = memberRepository.findAll();
 
         // then
         assertThat(result.size()).isEqualTo(2);
